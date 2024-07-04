0c5df4dfa824fe823c6c1dc55f1fe502e18e6abb
Toni Kostov <toni.kostov@study.thws.de>
2024-07-04 19:16:20 +0200

    add pwa and update server

--- Server/src/main.rs
+++ Server/src/main.rs
@@ -1,55 +1,22 @@
+mod models;
+
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use serde::{Deserialize, Serialize};
 use std::{fs, io::Result};
 use chrono::{Local, DateTime, Utc};
+
 use const_format::formatcp;
 use http::header::{HeaderValue};
 use http::header::LINK;
 use actix_cors::Cors;
+use crate::models::Backup;
 
-// Define a base directory for backups
 const SERVER_ADDRESS: &str = "127.0.0.1:8081";
 const SERVER_BASE_URL: &str = formatcp!("http://{}", SERVER_ADDRESS);
 const BACKUP_DIR: &str = "./backup";
 const BACKUP_FILE_NAME: &str = "latest.json";
-
-const BACKUP_FILE_PATH: &str = formatcp!("{}/{}", BACKUP_DIR, BACKUP_FILE_NAME);
-
+ const BACKUP_FILE_PATH: &str = formatcp!("{}/{}", BACKUP_DIR, BACKUP_FILE_NAME);
 const ROUTE_PATH: &str = "/entries";
-
-#[derive(Serialize, Deserialize)]
-struct Category {
-    key: i32,
-    name: String,
-    color: String,
-}
-
-#[derive(Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct QA {
-    key: i32,
-    question: String,
-    answer: String,
-    category_id: i32,
-}
-
-#[derive(Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct Backup {
-    categories: Vec<Category>,
-    qas: Vec<QA>,
-    last_updated: Option<DateTime<Utc>>,
-}
-
-impl Default for Backup {
-    fn default() -> Self {
-        Self {
-            categories: vec![],
-            qas: vec![],
-            last_updated: Some(Utc::now()),
-        }
-    }
-}
 
 fn build_link_header_value(rel: &str) -> String {
     format!("<{}{}> rel=\"{}\"", SERVER_BASE_URL, ROUTE_PATH, rel)
@@ -86,15 +53,19 @@ async fn post_entries(entry: web::Json<Backup>) -> impl Responder {
     }
 }
 
+fn config_routes(cfg: &mut web::ServiceConfig) {
+    cfg
+        .route("/", web::get().to(get_dispatcher))
+        .route(ROUTE_PATH, web::get().to(get_entries))
+        .route(ROUTE_PATH, web::post().to(post_entries));
+}
+
 #[actix_rt::main]
 async fn main() -> Result<()> {
     HttpServer::new(|| {
         App::new()
-            .wrap(Cors::permissive().allow_any_method())
-            .wrap(Cors::permissive().allow_any_header())
-            .route("/", web::get().to(get_dispatcher))
-            .route(ROUTE_PATH, web::get().to(get_entries))
-            .route(ROUTE_PATH, web::post().to(post_entries))
+            .wrap(Cors::permissive())
+            .configure(config_routes)
     })
         .bind("127.0.0.1:8081")?
         .run()
---
+++ Server/src/models.rs
@@ -0,0 +1,36 @@
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct Category {
+    pub key: i32,
+    pub name: String,
+    pub color: String,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct QA {
+    pub key: i32,
+    pub question: String,
+    pub answer: String,
+    pub category_id: i32,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Backup {
+    pub categories: Vec<Category>,
+    pub qas: Vec<QA>,
+    pub last_updated: Option<DateTime<Utc>>,
+}
+
+impl Default for Backup {
+    fn default() -> Self {
+        Self {
+            categories: vec![],
+            qas: vec![],
+            last_updated: Some(Utc::now()),
+        }
+    }
+}
